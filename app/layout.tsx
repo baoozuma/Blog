c7112ff39f1c893a3c22b434fc06ed3687950de2
aleksis-arendt <phambao0205@gmail.com>
2026-04-05 19:52:39 +0700

    lan cuoi

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,36 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        {[
+        // tags
+        '/tags/analysis-icon.jpg',
+        '/tags/analysis.jpg',
+        '/tags/diffgeo-icon.jpg',
+        '/tags/diffgeo.jpg',
+        '/tags/functional-icon.jpg',
+        '/tags/functional.jpg',
+        '/tags/GMT.jpg',
+        '/tags/measure-icon.jpg',
+        '/tags/measure.jpg',
+        '/tags/metric-icon.jpg',
+        '/tags/metric.jpg',
+        '/tags/olympiad.jpg',
+        '/tags/pde.jpg',
+        '/tags/physics-icon.jpg',
+        '/tags/physics.jpg',
+        // subareas
+        '/subareas/elliptic.jpg',
+        '/subareas/fractal-icon.jpg',
+        '/subareas/fractal.jpg',
+        '/subareas/minimal-icon.jpg',
+        '/subareas/minimal.jpg',
+        '/subareas/ricci-icon.jpg',
+        '/subareas/ricci.jpg',
+        '/subareas/variation-icon.jpg',
+        '/subareas/variation.jpg',
+      ].map(src => (
+        <link key={src} rel="preload" as="image" href={src} />
+      ))}
       </head>
       <body>
 
